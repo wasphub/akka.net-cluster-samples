e82045a65fb28f0f62ea16c61eab070d35db5c88
Roberto Vespa <roby@robychechi.it>
2019-12-01 12:08:39 +0100

    Startup boilerplate

---
+++ src/Beacon/Host.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using System.Threading.Tasks;
+
+namespace Beacon
+{
+    class Host
+    {
+        private readonly string _actorSystemName;
+
+        public ActorSystem System { get; private set; }
+
+        public Host() : this(actorSystemName: null) { }
+
+        public Host(IHostFactory factory)
+        {
+            Start(factory);
+        }
+
+        public Host(string actorSystemName)
+        {
+            _actorSystemName = actorSystemName;
+        }
+
+        public void Start(IHostFactory factory, int port = 0)
+        {
+            System = factory.Launch(_actorSystemName, port);
+        }
+
+        public Task TerminationHandle => System.WhenTerminated;
+
+        public async Task StopAsync()
+        {
+            var cluster = Akka.Cluster.Cluster.Get(System);
+            await cluster.LeaveAsync();
+
+            await CoordinatedShutdown.Get(System).Run(new StopReason());
+        }
+
+        class StopReason : CoordinatedShutdown.Reason { }
+    }
+}
---
+++ src/Beacon/IHostFactory.cs
@@ -0,0 +1,9 @@
+﻿using Akka.Actor;
+
+namespace Beacon
+{
+    public interface IHostFactory
+    {
+        ActorSystem Launch(string systemName = null, int port = 0);
+    }
+}
